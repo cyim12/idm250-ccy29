587a2c54610660f64abbc82ad10aa0d117a480b6
cyim12 <71360341+cyim12@users.noreply.github.com>
2022-02-27 20:44:55 -0500

    rename

--- wp-content/themes/idm250/404.php
+++ wp-content/themes/idm250/404.php
@@ -4,7 +4,7 @@
         <h1 class="h1-404">404</h1>
         <h3>Oops! Page not found.</h3>
         <br>
-        <p>Sorry, the page you're looking for doesn't exist, isn't available, or was loading incorrectly!</p>
+        <p class="p-404">Sorry, the page you're looking for doesn't exist, isn't available, or was loading incorrectly!</p>
         <br>
         <a href="http://localhost:8888/works/" class="button">VIEW WORKS</a>
       </div>
