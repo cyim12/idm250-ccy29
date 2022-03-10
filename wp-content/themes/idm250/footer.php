2b249b332f79aca868d726bb288623b5f33e4731
cyim12 <71360341+cyim12@users.noreply.github.com>
2022-03-09 23:02:21 -0500

    search

--- wp-content/themes/idm250/footer.php
+++ wp-content/themes/idm250/footer.php
@@ -7,7 +7,7 @@
         <br>
         <div class="connect">
           <a href="https://www.linkedin.com/in/christine-yim-b1ba33174" target="_blank"><img class="connect-icon" src="http://christineyim.com/idm250-ccy29/wp-content/uploads/2022/02/linkedin_icon.png"></a>
-          <a href="hhttps://www.instagram.com/cyim_designs/" target="_blank"><img class="connect-icon" src="http://christineyim.com/idm250-ccy29/wp-content/uploads/2022/02/instagram_icon.png"></a>
+          <a href="https://www.instagram.com/cyim_designs/" target="_blank"><img class="connect-icon" src="http://christineyim.com/idm250-ccy29/wp-content/uploads/2022/02/instagram_icon.png"></a>
           <a href="https://github.com/cyim12" target="_blank"><img class="connect-icon" src="http://christineyim.com/idm250-ccy29/wp-content/uploads/2022/02/github_icon.png"></a>
         </div>
         <div class="border-footer"></div>
