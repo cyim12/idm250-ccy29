72b5d4670d1fe8a74a835cff52c19cf641d631ae
cyim12 <71360341+cyim12@users.noreply.github.com>
2022-03-17 12:14:04 -0400

    nav menus

--- wp-content/themes/idm250/functions.php
+++ wp-content/themes/idm250/functions.php
@@ -1,16 +1,18 @@
 <?php
 
-
 // Check Server HP Version
 if (version_compare('7.4', phpversion(), '<')) {
     // die('You must be using PHP 7.4 or greater.');
 }
 
 
+
+
 //Check WP Version
 if (version_compare($GLOBALS['wp_version'], '5.4.2', '<')) {
     // die('WP theme only works in WordPress 5.4.2 or later. Please upgrade your WP site');
 }
+
 
 
 
@@ -33,7 +35,8 @@ add_action('wp_enqueue_scripts', 'include_styles');
 
 
 //Include any scripts into the site the proper way
-function include_js_files() {
+function include_js_files() 
+{
     wp_enqueue_script(
         'idm250-js', 
         get_template_directory_uri() . '/dist/scripts/main.js', 
@@ -43,6 +46,7 @@ function include_js_files() {
     );
 }
 
+//When WP performs this action, call our function
 add_action('wp_enqueue_scripts', 'include_js_files');
 
 
@@ -60,6 +64,8 @@ function register_theme_navigation()
 add_action('after_setup_theme', 'register_theme_navigation');
 
 
+
+
 //Render Custom Menu
 function idm_render_menu($menu_name)
 {
@@ -72,6 +78,8 @@ function idm_render_menu($menu_name)
     $menu_items = wp_get_nav_menu_items($menu->term_id, ['order' => 'DESC']);
     return $menu_items;
 }
+
+
 
 
 /*
